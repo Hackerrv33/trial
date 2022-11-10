1c08665bdbe463a2c98ba1a06bd6b9c39a9a0959
Hackerrv33 <105097203+Hackerrv33@users.noreply.github.com>
2022-11-10 09:52:19 +0545

    Update ahad.py

--- ahad.py
+++ ahad.py
@@ -1299,7 +1299,7 @@ def Subscraption():
 	key1=open('/data/data/com.termux/files/usr/bin/.mrahsan-cov', 'r').read()
 	clear()
 	print(logo)
-	r1=requests.get("https://github.com/Hackerrv33/mr.rahul/server.txt
+	r1=requests.get("https://pastebin.com/p3jbWM14").text
 	if key1 in r1:
 		os.system('clear')
 		print(logo)
